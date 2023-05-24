629f19c58c057c3c78db96e346ee88662ac2f659
oriyor <39461788+oriyor@users.noreply.github.com>
2023-05-24 22:01:06 +0300

    new bash scripts

--- bash_scripts/start_clients.sh
+++ bash_scripts/start_clients.sh
@@ -1,1 +1,2 @@
-nohup python3 -m fastchat.serve.gradio_web_server > logs/ui.log &
+nohup python3 -m fastchat.serve.gradio_web_server --share > logs/ui_401.log &
+nohup python3 -m fastchat.serve.openai_api_server --host localhost --port 8000 > logs/openai_401.log &
